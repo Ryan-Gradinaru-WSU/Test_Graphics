4d5a4ec430be940c600299b741e7c9b257b566f0
Ryan-Gradinaru-WSU <ryan.gradinaru@wsu.edu>
2026-02-09 19:54:33 -0800

    got gpu functionality working for linux vulkan - theoretically multiplatform

--- src/Window/BaseSDL.cpp
+++ src/Window/BaseSDL.cpp
@@ -1,4 +1,5 @@
 #include "./../../headers/Window/BaseSDL.h"
+#include <SDL3/SDL_video.h>
 
 #define WINDOW_WIDTH    1280
 #define WINDOW_HEIGHT   800
@@ -9,10 +10,8 @@ BaseSDL::BaseSDL( Uint32 flags )
     {
         throw InitError();
     }
-
-    if (!SDL_CreateWindowAndRenderer( "fluid sim", WINDOW_WIDTH, WINDOW_HEIGHT, 
-                                      SDL_EVENT_WINDOW_SHOWN,
-                                      &m_window, &m_renderer ) != 0)
+    m_window = SDL_CreateWindow("fortnite", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_HIGH_PIXEL_DENSITY);
+    if (!m_window)
     {
         throw InitError();
     }
@@ -21,15 +20,21 @@ BaseSDL::BaseSDL( Uint32 flags )
 BaseSDL::~BaseSDL()
 {
     SDL_DestroyWindow( m_window );
-    SDL_DestroyRenderer( m_renderer );
     SDL_Quit();
 }
 
+// TEMP FUNCTION - STAGE WILL DRAW
 void BaseSDL::draw()
 {
+    
+    
+    
+    // TEMP CODE
+
+
     // Clear the window with a black background
-    SDL_SetRenderDrawColor( m_renderer, 0, 0, 0, 255 );
-    SDL_RenderClear( m_renderer );
+    //SDL_SetRenderDrawColor( m_renderer, 0, 0, 0, 255 );
+    //SDL_RenderClear( m_renderer );
 
     int rgb[] = { 203, 203, 203, // Gray
                   254, 254,  31, // Yellow
@@ -49,17 +54,17 @@ void BaseSDL::draw()
 
     // Render a new color bar every 0.5 seconds
 
-    SDL_SetRenderDrawColor( m_renderer, rgb[0], rgb[0 + 1], rgb[0 + 2], 255 );
-    SDL_RenderFillRect( m_renderer, &colorBar );
+    //SDL_SetRenderDrawColor( m_renderer, rgb[0], rgb[0 + 1], rgb[0 + 2], 255 );
+    //SDL_RenderFillRect( m_renderer, &colorBar );
 
 
     for ( int i = 0; i != sizeof rgb / sizeof * rgb; i += 3)
     {
         colorBar.x += barWidth;
-        SDL_SetRenderDrawColor( m_renderer, rgb[i], rgb[i + 1], rgb[i + 2], 255 );
-        SDL_RenderFillRect( m_renderer, &colorBar );
+    //    SDL_SetRenderDrawColor( m_renderer, rgb[i], rgb[i + 1], rgb[i + 2], 255 );
+    //    SDL_RenderFillRect( m_renderer, &colorBar );
     }
 
-    SDL_RenderPresent( m_renderer );
+    //SDL_RenderPresent( m_renderer );
     
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,101 @@
 #include "./../headers/Window/BaseSDL.h"
 #include <SDL3/SDL_error.h>
+#include <SDL3/SDL_gpu.h>
+#include <SDL3/SDL_init.h>
 
 int main(int argc, char* argv[])
 {
     bool quit = false;
+    /* block: temp code */
+    //SDL_SetHint(SDL_HINT_GPU_DRIVER, "vulkan");
+    std::cout << "SDL3 lib path: " << SDL_GetBasePath() << std::endl;
+
+    std::cout 
+        << "VIDEO INIT SUCCEESS: "
+        << (SDL_Init(SDL_INIT_VIDEO) ? "true" : "false")
+        << std::endl;
+
+    std::cout 
+        << SDL_GetError()
+        << std::endl;
+
+
+    SDL_Log("GPU DRIVER COUNT: %d\n", SDL_GetNumGPUDrivers());
+    SDL_Log("SDL GET GPU DRIVER: %s\n", SDL_GetGPUDriver(0));
+
+    //SDL_Vulkan_LoadLibrary(nullptr);
+    SDL_GPUDevice* test_dev;
+    test_dev = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL, true, NULL);
+
+
+    SDL_GPUTextureCreateInfo test_texInfo;
+    test_texInfo.format = SDL_GPU_TEXTUREFORMAT_R32G32B32A32_FLOAT;
+
+    std::cout 
+        << SDL_GetError()
+        << std::endl;
+
+    std::cout
+        << "DEVICE EXISTS: "
+        << (test_dev ? "yes" : "no")
+        << std::endl;
+
+    std::cout 
+        << "IS DEVICE FORMAT SUPPORTED: "
+        << (SDL_GPUSupportsShaderFormats(4, NULL) ? "yes" : "no")
+        << std::endl;
+
+    std::cout   
+        << "IS TEXTURE FORMAT SUPPORTED: " 
+        << (SDL_GPUTextureSupportsFormat(test_dev, test_texInfo.format, SDL_GPU_TEXTURETYPE_2D, 0) ? "yes" : "no")
+        << std::endl;
+    
+    //    test_texInfo.height;
+    SDL_GPUTexture* test_tex;
+    //test_tex = SDL_CreateGPUTexture(test_dev, )
+
+    /* end block */
 
     try
     {
         BaseSDL b_sdl( SDL_INIT_VIDEO);
+
+
+        /* block: temp code */
+
+        if(!SDL_ClaimWindowForGPUDevice(test_dev, b_sdl.m_window)){
+            std::cout 
+                << SDL_GetError()
+                << std::endl;
+            return 0;
+        }
+
+        SDL_GPUCommandBuffer* test_buff = SDL_AcquireGPUCommandBuffer(test_dev);
+
+        SDL_GPUTexture* backbuffer;
+        Uint32 w, h;
+
+        if(!SDL_AcquireGPUSwapchainTexture(test_buff, b_sdl.m_window, &backbuffer, &w, &h)){
+            std::cout 
+                << SDL_GetError()
+                << std::endl;
+            return 0;
+        }
+
+        SDL_GPUColorTargetInfo color_target = {};
+            color_target.texture = backbuffer;
+            color_target.clear_color = {1.0f, 0, 1.0f, 1.0f};
+            color_target.load_op = SDL_GPU_LOADOP_CLEAR;
+            color_target.store_op = SDL_GPU_STOREOP_STORE;
+        
+        SDL_GPUDepthStencilTargetInfo* test_dstarget;
+
+        SDL_GPURenderPass* test_rp = SDL_BeginGPURenderPass(test_buff, &color_target, 1, test_dstarget);
+
+        SDL_EndGPURenderPass(test_rp);
+        SDL_SubmitGPUCommandBuffer(test_buff);
+        /* end block */
+
 
         while(!quit)
         {
@@ -30,7 +118,8 @@ int main(int argc, char* argv[])
                         break;
                     }
                 }
-                b_sdl.draw();
+                //TEMP REMOVE
+                //b_sdl.draw();
             }
         }
 
