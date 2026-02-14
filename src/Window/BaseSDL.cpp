d3df169bb70681192735d60b111f8c54f42a556a
Ryan-Gradinaru-WSU <ryan.gradinaru@wsu.edu>
2026-02-13 16:02:29 -0800

    fixed an error where new frames were only rendered on events, causing framerate to reach 2 fps. framerate should now be uncapped

--- src/Window/BaseSDL.cpp
+++ src/Window/BaseSDL.cpp
@@ -1,20 +1,45 @@
 #include "./../../headers/Window/BaseSDL.h"
+#include <SDL3/SDL_gpu.h>
+#include <SDL3/SDL_pixels.h>
+#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 
 #define WINDOW_WIDTH    1280
 #define WINDOW_HEIGHT   800
 
+
 BaseSDL::BaseSDL( Uint32 flags )
 {
-    if (!SDL_Init( flags ) != 0)
+    if(!SDL_Init(flags))
     {
         throw InitError();
     }
-    m_window = SDL_CreateWindow("fortnite", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_HIGH_PIXEL_DENSITY);
+    DEBUG_PRINT("SDL_INIT SUCCESS!");
+    //SDL_Vulkan_LoadLibrary(nullptr);
+
+    m_window = SDL_CreateWindow("fortnite", WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     if (!m_window)
     {
         throw InitError();
     }
+    DEBUG_PRINT("WINDOW CREATION SUCCESS!");
+
+
+    m_gpuDevice = SDL_CreateGPUDevice(SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL, true, NULL);
+
+
+    if (!m_gpuDevice)
+    {
+        throw InitError();
+    }
+    DEBUG_PRINT("GPU DEVICE CREATION SUCCESS!");
+
+    runInitTests();
+
+    if(!SDL_ClaimWindowForGPUDevice(m_gpuDevice, m_window)){
+        throw InitError();
+    }
+    DEBUG_PRINT("GPU BIND SUCCESS!");
 }
 
 BaseSDL::~BaseSDL()
@@ -23,48 +48,77 @@ BaseSDL::~BaseSDL()
     SDL_Quit();
 }
 
+bool BaseSDL::runInitTests(){
+    int num_drivers = SDL_GetNumGPUDrivers();
+    DEBUG_PRINT(num_drivers << " driver(s) supported on this machine!");
+
+    std::string supported_driver = SDL_GetGPUDriver(0);
+    DEBUG_PRINT(supported_driver << " will be used on this machine!");
+    
+    if(SDL_GPUSupportsShaderFormats(SDL_GPU_SHADERFORMAT_SPIRV | SDL_GPU_SHADERFORMAT_DXIL, NULL))
+        DEBUG_PRINT("D3X12 shaders and Vulkan shaders supported!");
+    else{
+        DEBUG_PRINT("ERROR - SHADERS NOT SUPPORTED.");
+        return false;
+    }
+
+
+    if(SDL_GPUTextureSupportsFormat(m_gpuDevice, SDL_GPU_TEXTUREFORMAT_R32G32B32A32_FLOAT, SDL_GPU_TEXTURETYPE_2D, 0) ? "yes" : "no")
+        DEBUG_PRINT("R32G32B32A32_FLOAT supported for 2D textures!");
+    else{
+        DEBUG_PRINT("ERROR - COMPLEX TEXTURE FORMAT NOT SUPPORTED.");
+        return false;
+    }
+
+    if(SDL_GPUTextureSupportsFormat(m_gpuDevice, SDL_GPU_TEXTUREFORMAT_D16_UNORM, SDL_GPU_TEXTURETYPE_2D, 0) ? "yes" : "no")
+        DEBUG_PRINT("D16_UNORM supported for 2D textures!");
+    else{
+        DEBUG_PRINT("ERROR - DEPTH TEXTURE FORMAT NOT SUPPORTED.");
+        return false;
+    }
+
+    return true;
+}
+
 // TEMP FUNCTION - STAGE WILL DRAW
 void BaseSDL::draw()
 {
-    
-    
-    
-    // TEMP CODE
+    SDL_GPUCommandBuffer* test_cmdbuff = SDL_AcquireGPUCommandBuffer(m_gpuDevice);
 
-
-    // Clear the window with a black background
-    //SDL_SetRenderDrawColor( m_renderer, 0, 0, 0, 255 );
-    //SDL_RenderClear( m_renderer );
-
-    int rgb[] = { 203, 203, 203, // Gray
-                  254, 254,  31, // Yellow
-                    0, 255, 255, // Cyan
-                    0, 254,  30, // Green
-                  255,  16, 253, // Magenta
-                  253,   3,   2, // Red
-                   18,  14, 252, // Blue
-                    0,   0,   0  // Black
-                };
-
-    SDL_FRect colorBar;
-    double barWidthRatio = 90.0/640.0;
-    int barWidth = barWidthRatio * WINDOW_WIDTH;
-
-    colorBar.x = 0; colorBar.y = 0; colorBar.w = barWidth; colorBar.h = WINDOW_HEIGHT;
-
-    // Render a new color bar every 0.5 seconds
-
-    //SDL_SetRenderDrawColor( m_renderer, rgb[0], rgb[0 + 1], rgb[0 + 2], 255 );
-    //SDL_RenderFillRect( m_renderer, &colorBar );
-
-
-    for ( int i = 0; i != sizeof rgb / sizeof * rgb; i += 3)
-    {
-        colorBar.x += barWidth;
-    //    SDL_SetRenderDrawColor( m_renderer, rgb[i], rgb[i + 1], rgb[i + 2], 255 );
-    //    SDL_RenderFillRect( m_renderer, &colorBar );
+    if(!test_cmdbuff){
+        throw InitError();
     }
 
-    //SDL_RenderPresent( m_renderer );
+    SDL_GPUTexture* backbuffer;
+    Uint32 w, h;
+
+    if(!SDL_WaitAndAcquireGPUSwapchainTexture(test_cmdbuff, m_window, &backbuffer, &w, &h)){
+        throw InitError();
+    }
+
+    if(!test_cmdbuff){
+        throw InitError();
+    }
+
+    if (w == 0 || h == 0) {
+        SDL_CancelGPUCommandBuffer(test_cmdbuff);
+        return; // skip this frame
+    }
     
+
+    SDL_GPUColorTargetInfo color_target = {
+        .texture = backbuffer,
+        .clear_color = (SDL_FColor){1.0f, 0, 1.0f, 1.0f},
+        .load_op = SDL_GPU_LOADOP_CLEAR,
+        .store_op = SDL_GPU_STOREOP_DONT_CARE
+    };
+    
+    SDL_GPURenderPass* test_rp = SDL_BeginGPURenderPass(test_cmdbuff, &color_target, 1, NULL);
+
+    if(!test_rp){
+        throw InitError();
+    }
+
+    SDL_EndGPURenderPass(test_rp);
+    SDL_SubmitGPUCommandBuffer(test_cmdbuff);
 }
